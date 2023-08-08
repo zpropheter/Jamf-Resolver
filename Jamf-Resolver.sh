5baadaa1b3c98014ce03a3e0372b56e08e35ee5d
zpropheter <128726098+zpropheter@users.noreply.github.com>
2023-08-08 07:12:20 -0700

    Create Jamf-Resolver.sh
    Initial creation of Jamf-Resolver

---
+++ Jamf-Resolver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+#Jamf-Resolver is designed to walk admins and end-users through resolutions to common issues. It starts with some of the most common issues and progresses into some of the more complex items from there
